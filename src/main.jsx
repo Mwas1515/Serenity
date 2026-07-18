63276b78dbd5396221f66dd4ebbf108c602963e5
Mwas1515 <deniswambui6@gmail.com>
2026-07-19 02:12:37 +0300

    feat:add theme provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <AuthProvider>
-   <BrowserRouter>
-    <App />
-   </BrowserRouter>
-   </AuthProvider>
-  </StrictMode>,
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
