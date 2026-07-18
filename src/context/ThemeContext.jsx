e5d5cbc8b998e22e144b93382eb5f52a24f0fda5
Mwas1515 <deniswambui6@gmail.com>
2026-07-19 02:14:06 +0300

    feat: add user preference to themecontext

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,9 +3,15 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") || "light";
-  });
+const [theme, setTheme] = useState(() => {
+  const saved = localStorage.getItem("theme");
+
+  if (saved) return saved;
+
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+});
 
   useEffect(() => {
     const html = document.documentElement;
